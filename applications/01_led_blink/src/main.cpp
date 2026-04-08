ec9a9181536bc6952f52665e01d39c936601fdbc
micartey <me@micartey.dev>
2026-04-08 15:46:19 +0200

    sub commands

--- applications/01_led_blink/src/main.cpp
+++ applications/01_led_blink/src/main.cpp
@@ -10,7 +10,19 @@ static int cmd_hello(const struct shell *sh, int argc, char **argv) {
   return 0;
 }
 
-SHELL_CMD_REGISTER(hello, NULL, "Print \"World!\"", cmd_hello);
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
+        SHELL_CMD(ping, NULL, "Ping command.", +[](const struct shell *sh, int argc, char **argv) -> int {
+            ARG_UNUSED(sh);
+            ARG_UNUSED(argc);
+            ARG_UNUSED(argv);
+
+            printk("Pong!\n");
+            return 0;
+        }),
+        SHELL_SUBCMD_SET_END
+);
+
+SHELL_CMD_REGISTER(hello, &sub_demo, "Print \"World!\"", cmd_hello);
 
 int main(void) {
   const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
