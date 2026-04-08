8cd8335040f1d687a2764818007cef01b287bacc
micartey <me@micartey.dev>
2026-04-08 15:31:08 +0200

    hello command

--- applications/01_led_blink/src/main.cpp
+++ applications/01_led_blink/src/main.cpp
@@ -1,7 +1,16 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+#include "zephyr/sys/printk.h"
 
 #include "bootloader_cmd.h"
+
+static int cmd_hello(const struct shell *sh, int argc, char **argv) {
+  printk("World!\n");
+  return 0;
+}
+
+SHELL_CMD_REGISTER(hello, NULL, "Print \"World!\"", cmd_hello);
 
 int main(void) {
   const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
